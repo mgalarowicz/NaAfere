c6f2cb748708850b911c66c3bc75227b82167ab4
mgalarowicz <marek.galarowicz@enova.pl>
2019-10-22 23:28:13 +0200

    location Component

--- NaAfere-SPA/src/app/location/location.component.ts
+++ NaAfere-SPA/src/app/location/location.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import OlMap from 'ol/Map';
+import OlXYZ from 'ol/source/XYZ';
+import OlTileLayer from 'ol/layer/Tile';
+import OlView from 'ol/View';
+
+import { fromLonLat } from 'ol/proj';
+
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
@@ -7,9 +14,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationComponent implements OnInit {
 
+  map: OlMap;
+  source: OlXYZ;
+  layer: OlTileLayer;
+  view: OlView;
+
   constructor() { }
 
   ngOnInit() {
-  }
+    this.source = new OlXYZ({
+      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
+    });
 
+    this.layer = new OlTileLayer({
+      source: this.source
+    });
+
+    this.view = new OlView({
+      center: fromLonLat([6.661594, 50.433237]),
+      zoom: 3
+    });
+
+    this.map = new OlMap({
+      target: 'map',
+      layers: [this.layer],
+      view: this.view
+    });
+}
 }
